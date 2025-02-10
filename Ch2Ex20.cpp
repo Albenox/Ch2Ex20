96853aa2535d97eb1da8c7ac9d7c4511d695cb4c
Gabriel Inocentes <gabriel.inocentes@gmail.com>
2025-02-10 00:00:00 -0500

    Allows user to input custom dimensions for fence

--- Ch2Ex20.cpp
+++ Ch2Ex20.cpp
@@ -9,12 +9,19 @@
 #include <iostream>
 using namespace std;
 
-int const height = 6;
-int const length = 100;
 int const paintCoverage = 340;
 
 int main()
 {
+    int height;
+    int length;
+
+    cout << "How tall is the fence in feet?" << endl;
+    cin >> height;
+
+    cout << "How long is the fence in feet?" << endl;
+    cin >> length;
+    
     int paintCans = (height * length * 2) / paintCoverage;
     cout << "You will need approximately " << paintCans << " paint cans to cover " << height * length << " square feet of fence with two layers.";
 }
